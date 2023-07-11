762c09bc81520a426f20f087d69bb16c2c4873b0
saeyeon <saeyeon220@daum.net>
2023-07-12 00:07:15 +0900

    feat: 우주선 움직이게 하기

--- shooting-game/main.js
+++ shooting-game/main.js
@@ -27,6 +27,37 @@ function loadImage() {
   gameoverImage.src = 'images/game-over.png';
 }
 
+let keysDown = {};
+// 키보드 눌렀을때 함수
+function setupKeyboardListner() {
+  // 눌렀을때 keysDown에 계속 추가
+  document.addEventListener('keydown', function (event) {
+    keysDown[event.keyCode] = true;
+    // console.log('키다운 객체에 들어간 값은?', keysDown);
+  });
+  // 때면 keysDown 삭제
+  document.addEventListener('keyup', function (event) {
+    delete keysDown[event.keyCode];
+    // console.log('버튼클릭후', keysDown);
+  });
+}
+
+function update() {
+  if (39 in keysDown) {
+    //right:39
+    spaceshipX += 5; //우주선의 속도
+  } else if (37 in keysDown) {
+    //left:37
+    spaceshipX -= 5;
+  }
+  if (spaceshipX <= 0) {
+    spaceshipX = 0;
+  }
+  if (spaceshipX >= canvas.width - 64) {
+    spaceshipX = canvas.width - 64;
+  }
+}
+
 // 이미지를 보여주는 함수
 function render() {
   // drawImage(image, dx, dy, dWidth, dHeight)
@@ -40,6 +71,8 @@ function main() {
   // console.log('animation calls main function');
   // 애니메이션처럼 함수를 계속 호출해줌
   requestAnimationFrame(main);
+  update();
 }
 loadImage();
+setupKeyboardListner();
 main();
